c428ed59a1272b6a5439b8f1f649795548a8bed5
pavkar <pav.kar97@gmail.com>
2022-02-04 12:48:31 +0200

    Jar file creation with dependecies

--- src/main/java/ee/eid/installer/CapFileInstaller.java
+++ src/main/java/ee/eid/installer/CapFileInstaller.java
@@ -4,7 +4,7 @@ public class CapFileInstaller {
     public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("To install application choose specific file.");
-            System.out.println("Format: cap-file-installer {filename}");
+            System.out.println("Format: password_storage_applet-0.1-install.jar {filename}");
         }
         System.out.println("Installation of the CAP file in progress!");
 
